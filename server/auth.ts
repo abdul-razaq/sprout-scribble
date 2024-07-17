1621eeca70a91f9d3af7e0e667113c2d346d25c4
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2024-07-17 01:13:01 +0100

    added jwt strategy to the auth file

--- server/auth.ts
+++ server/auth.ts
@@ -7,6 +7,7 @@ import Github from "next-auth/providers/github"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db) as any,
   secret: process.env.AUTH_SECRET,
+  session: { strategy: 'jwt'},
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
